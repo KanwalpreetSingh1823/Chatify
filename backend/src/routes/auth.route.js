c2c16c6f362a028b68b4f911dec847b90fa881d1
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-12-24 23:46:15 +0530

    Update Profile endpoint created for user to update Profile after creation

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {signup, login, logout} from "../controllers/auth.controller.js";
+import {signup, login, logout, updateProfile} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.post("/update-profile",protectRoute, updateProfile);
+// this route is protected, which means if the user is logged in / authenticated --> then only he/she can update the profile.
 
 export default router;
