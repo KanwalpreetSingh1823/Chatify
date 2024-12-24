93b95857e220a9709c34bb96be637dd776623e15
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-12-24 23:25:36 +0530

    Middleware Created for protecting routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,16 @@ import dotenv from "dotenv";
 
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser"
 
 dotenv.config();
 const app = express();
 // Creating the first route for authentication
 
-app.use(express.json()) // middleware
+app.use(express.json()) // it will allow to parse the json
 app.use("/api/auth", authRoutes)
+
+app.use(cookieParser()); // it will allow to parse the cookies
 
 const PORT = process.env.PORT;
 
