406166898476a4db2c4adb1b49b86b44e56d5895
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2024-12-24 20:29:46 +0530

    Create SignUp endpoint for user registration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-const app = express();
 
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+const app = express();
 // Creating the first route for authentication
+
+app.use(express.json()) // middleware
 app.use("/api/auth", authRoutes)
 
 const PORT = process.env.PORT;
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on PORT : ${PORT}`);
     connectDB();
