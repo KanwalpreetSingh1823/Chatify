b36f8692a107deeefa26b675f5d85b3439d3893b
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2025-01-04 20:28:44 +0530

    Implement a method to send a message

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -31,3 +31,36 @@ export const getMessages = async(req, res) =>{
         res.status(500).json({error : "Internal server error"});
     }
 }
+
+// Method to send a message
+export const sendMessage = async (req, res) =>{
+    try {
+        const {text, image} = req.body;
+        const {id : receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            // Upload base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image : imageUrl
+        })
+
+        await newMessage.save();
+
+        // Real time functionality will implement here
+        // Implement using socket.io 
+
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.error("Error in sendMessage controller : ", error.message);
+        res.status(500).json({error : "Internal server error"});
+    }
+}
