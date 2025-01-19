f6da7cf6e4681b5ac408436861f3055ef63ab78d
jasmeensingh <jasmeensingh264@gmail.com>
2025-01-19 16:48:20 +0530

    ui of settings page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import ProfilePage from "./Pages/ProfilePage.jsx"
 import {Routes, Route, Navigate} from "react-router-dom"
 import { useAuthStore } from './store/useAuthStore.js'
 import { useThemeStore } from "./store/useThemeStore";
-import { useEffect } from "react";
+
 import { Loader } from "lucide-react"
 import {Toaster} from "react-hot-toast"
 
