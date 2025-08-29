c5574a3209f1abbe3f7831eeb3ba191e14aa2570
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2025-08-29 11:25:23 +0530

    [ Frontend ] | Create Message Skeleton component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,52 @@
-import React, { useEffect } from 'react'
-import Navbar from "./Components/Navbar.jsx"
-import HomePage from "./Pages/HomePage.jsx"
-import SignUpPage from "./Pages/SignUpPage.jsx"
-import LoginPage from "./Pages/LoginPage.jsx"
-import SettingsPage from "./Pages/SettingsPage.jsx"
-import ProfilePage from "./Pages/ProfilePage.jsx"
+import Navbar from "./components/Navbar";
 
-import {Routes, Route, Navigate} from "react-router-dom"
-import { useAuthStore } from './store/useAuthStore.js'
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
+import { useEffect } from "react";
 
-import { Loader } from "lucide-react"
-import {Toaster} from "react-hot-toast"
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
 
-  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
-  const{theme}=useThemeStore()
-  useEffect(()=>{
+  console.log({ onlineUsers });
+
+  useEffect(() => {
     checkAuth();
-  }, [checkAuth])
+  }, [checkAuth]);
 
-  console.log({authUser});
+  console.log({ authUser });
 
-  if(isCheckingAuth && !authUser){
-    <div className='flex items-center justify-center h-screen'>
-      <Loader className="spin-10 animate-spin"/>
-    </div>
-  }
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
     <div data-theme={theme}>
-      <Navbar/>
+      <Navbar />
+
       <Routes>
-        <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
-        <Route path='/signup' element={!authUser ? <SignUpPage/> : <Navigate to="/"/> }/>
-        <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
-        <Route path='/settings' element={<SettingsPage/>}/>
-        <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to="/login"/>}/>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
 
-      <Toaster/>
+      <Toaster />
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
