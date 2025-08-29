9906ab224b215d28032147b0e6b219a1abf1b346
KanwalpreetSingh1823 <kanwalpreets17@gmail.com>
2025-08-29 11:22:54 +0530

    [ Backend ] | Implement WebSocket in send message controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,11 @@
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
+
+import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
+
+
 export const getUsersForSidebar = async(req, res) =>{
   try {
     const loggedInUserId = req.user._id;
@@ -33,34 +38,36 @@ export const getMessages = async(req, res) =>{
 }
 
 // Method to send a message
-export const sendMessage = async (req, res) =>{
-    try {
-        const {text, image} = req.body;
-        const {id : receiverId} = req.params;
-        const senderId = req.user._id;
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-        let imageUrl;
-        if(image){
-            // Upload base64 image to cloudinary
-            const uploadResponse = await cloudinary.uploader.upload(image);
-            imageUrl = uploadResponse.secure_url;
-        }
-
-        const newMessage = new Message({
-            senderId,
-            receiverId,
-            text,
-            image : imageUrl
-        })
-
-        await newMessage.save();
-
-        // Real time functionality will implement here
-        // Implement using socket.io 
-
-        res.status(201).json(newMessage);
-    } catch (error) {
-        console.error("Error in sendMessage controller : ", error.message);
-        res.status(500).json({error : "Internal server error"});
+    let imageUrl;
+    if (image) {
+      // Upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
     }
-}
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
